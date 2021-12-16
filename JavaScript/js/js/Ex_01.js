25eccddab0759c7a262dad743cd8a651c2c12135
JessicaRogalCaldas <jessica.rogal21@gmail.com>
2021-12-15 23:35:48 -0300

    operações

---
+++ JavaScript/js/js/Ex_01.js
@@ -0,0 +1,15 @@
+var n1,n2,soma,sub,mult,div
+n1=prompt("Digite o primeiro valor: ")
+n2=prompt("Digite o segundo valor: ")
+soma=parseFloat(n1)+parseFloat(n2)
+alert("A soma é: " +soma)
+sub=parseFloat(n1)-parseFloat(n2)
+alert("A subtração é: " +sub)
+mult=parseFloat(n1)*parseFloat(n2)
+alert("A multiplicação é: " +mult)
+div=parseFloat(n1)*parseFloat(n2)
+alert("A divisão é: " +div)
+
+
+
+
